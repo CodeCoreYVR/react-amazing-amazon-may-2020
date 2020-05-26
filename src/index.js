9bcf104f7ff6aa1b6818096b379096322c47da08
Brandon Y Lam <me@brandonlam.ca>
2020-05-26 14:28:15 -0700

    [Lab] Amazon React: Product Show Page & [Lab] Amazon react: Review List

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import ProductDetails from './components/ProductDetails';
-import ReviewDetails from './components/ReviewDetails';
-
-import prodcutData from './data/product';
-import reviewData from './data/review';
+import ProductShowPage from './components/ProductShowPage';
 
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ProductDetails {...prodcutData}/>
-    <ReviewDetails {...reviewData}/>
+    <ProductShowPage />
   </React.StrictMode>,
   document.getElementById('root')
 );
