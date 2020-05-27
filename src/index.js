503876d386305d9f1f477a6119f8d1e29f796ef1
Brandon Y Lam <me@brandonlam.ca>
2020-05-27 13:49:09 -0700

    [Lab] Amazon React: Product Index & Product Show State

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import ProductShowPage from './components/ProductShowPage';
+import ProductIndexPage from './components/ProductIndexPage';
 
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <ProductShowPage />
+    <ProductIndexPage />
   </React.StrictMode>,
   document.getElementById('root')
 );
