bddbd608b45bcb11ed3094903d26fa726a2c9466
Anson Lam <anson.lam94@gmail.com>
2020-08-28 12:19:44 -0700

    [Review] Amazon React: Convert App to a Functional Component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ProductIndexPage from '../ProductIndexPage';
 import ProductShowPage from '../ProductShowPage';
@@ -8,71 +8,58 @@ import NavBar from '../NavBar';
 import AuthRoute from '../AuthRoute';
 import { Session, User } from '../../requests';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentUser: null,
-      isLoading: true,
-    }
-    this.getCurrentUser = this.getCurrentUser.bind(this);
-    this.destroySession = this.destroySession.bind(this);
-  }
+function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-  componentDidMount() {
-    this.getCurrentUser();
-    
-  }
+  useEffect(() => {
+    getCurrentUser();
+  }, [])
 
-  getCurrentUser() {
+  function getCurrentUser() {
     User.current().then(user => {
       if (user.id) {
-        this.setState({ 
-          currentUser: user,
-        });
+        setCurrentUser(user)
       }
-      this.setState({ isLoading: false });
+      setIsLoading(false)
     })
   }
 
-  destroySession() {
-    Session.destroy().then(() => this.setState({ currentUser: null }));
+  function destroySession() {
+    Session.destroy().then(() => setCurrentUser(null));
   }
 
-  render() {
-    const { currentUser, isLoading } = this.state;
-    return isLoading ? <main>Loading...</main> : (
-      <BrowserRouter>
-        <NavBar currentUser={currentUser} destroySession={this.destroySession} />
-        <Switch>
-          <Route 
-            path='/login' 
-            exact 
-            render={routeProps => (
-              <SignInPage {...routeProps} getCurrentUser={this.getCurrentUser} />
-            )}
-          />
-          <AuthRoute 
-            isAuthenticated={currentUser}
-            path='/products' 
-            exact 
-            component={ ProductIndexPage }
-          />
-          <AuthRoute 
-            isAuthenticated={currentUser}
-            path='/products/new' 
-            exact
-            component={ NewProductPage } 
-          />
-          <AuthRoute 
-            isAuthenticated={currentUser}
-            path='/products/:id' 
-            component={ ProductShowPage } 
-          />
-        </Switch>
-      </BrowserRouter>
-    )
-  }
+  return isLoading ? <main>Loading...</main> : (
+    <BrowserRouter>
+      <NavBar currentUser={currentUser} destroySession={destroySession} />
+      <Switch>
+        <Route 
+          path='/login' 
+          exact 
+          render={routeProps => (
+            <SignInPage {...routeProps} getCurrentUser={getCurrentUser} />
+          )}
+        />
+        <AuthRoute 
+          isAuthenticated={currentUser}
+          path='/products' 
+          exact 
+          component={ ProductIndexPage }
+        />
+        <AuthRoute 
+          isAuthenticated={currentUser}
+          path='/products/new' 
+          exact
+          component={ NewProductPage } 
+        />
+        <AuthRoute 
+          isAuthenticated={currentUser}
+          path='/products/:id' 
+          component={ ProductShowPage } 
+        />
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
 export default App
